d9c60e4ab2089fee7bc78d867c7cf5e303f79dce
Geoffrey <1183681473@qq.com>
2020-06-23 10:37:25 +0800

    struct

--- 第3章 面向对象编程/3.1 类型系统/3.1.3 结构体/main.go
+++ 第3章 面向对象编程/3.1 类型系统/3.1.3 结构体/main.go
@@ -7,6 +7,19 @@ package main
 	组合。组合只是形成复合类型的基础。
 	上面我们说到，所有的Go语言类型（指针类型除外）都可以有自己的方法。在这个背景下，
 	Go语言的结构体只是很普通的复合类型，平淡无奇。例如，我们要定义一个矩形类型：
+
+	type Rect struct {
+	x, y float64
+	width, height float64
+	}
+
+	然后我们定义成员方法Area()来计算矩形的面积：
+
+	func (r *Rect) Area() float64 {
+	return r.width * r.height
+	}
+
+	可以看出， Go语言中结构体的使用方式与C语言并没有明显不同
 */
 func main() {
 
---
+++ 第3章 面向对象编程/3.2 初始化/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Rect struct {
+	x,y float64
+	width,height float64
+}
+
+func (r *Rect)Area()float64{
+	return r.width * r.height
+}
+
+/*
+在Go语言中没有构造函数的概念，对象的创建通常交由一个全局的创建函数来完成，以
+NewXXX来命名，表示“构造函数”：
+*/
+func NewRect(x,y,width,height float64) *Rect{
+    return &Rect{x,y,width,height}
+}
+/*
+    这一切非常自然，开发者也不需要分析在使用了new之后到底背后发生了多少事情。在Go
+	语言中，一切要发生的事情都直接可以看到。
+	在Go语言中，未进行显式初始化的变量都会被初始化为该类型的零值，例如bool类型的零
+	值为false，int类型的零值为0，string类型的零值为空字符串。
+*/
+func main(){
+   rect1:=new (Rect)
+   fmt.Println(rect1)
+   rect2:=&Rect{0,0,100,200}
+   rect3:=&Rect{width: 100, height: 200}
+   fmt.Println(rect1,rect2,rect3)
+   fmt.Println(NewRect(0,0,0,0))
+   
+}
